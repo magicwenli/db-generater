9e889f01418b9712e1c462784a7231d3673e5620
magicwenli <yxra3603@outlook.com>
2021-04-27 15:27:31 +0800

    增加课程样本

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from models import db, base, JS020, JC020, JSC020
 
 DEBUG_MODE = 0
 DROP_ALL = 1
-STUDENT_NUM = 20
+STUDENT_NUM = 1000
 
 
 def insertFake(my_fake, json_path):
+    counter = [0, 0, 0]
     students_no = []  # 保存学号，添加选课表时会用
     # fake student 1000 名
     for i in range(STUDENT_NUM):
         s_info = my_fake.student()
         students_no.append(s_info["sno"])
         logger.info('student to be inserted: {}'.format(s_info))
+
+        counter[0] += 1
         if DEBUG_MODE == 1:
             pass
         else:
@@ -23,11 +26,13 @@ def insertFake(my_fake, json_path):
             session.add(student)
             session.commit()
 
-    # fake course
+    # fake course 228
     courses_info, courses_no = my_fake.course(json_path)
     # courses_no 保存课程号，添加选课表时会用
     for c_info in courses_info:
         logger.info('course to be inserted: {}'.format(c_info))
+
+        counter[1] += 1
         if DEBUG_MODE == 1:
             pass
         else:
@@ -54,6 +59,8 @@ def insertFake(my_fake, json_path):
                 fake_sc["grade"] = "{:.1f}".format(grade)
 
             logger.info('sc to be inserted: {}'.format(fake_sc))
+
+            counter[2] += 1
             if DEBUG_MODE == 1:
                 pass
             else:
@@ -61,6 +68,8 @@ def insertFake(my_fake, json_path):
                 sc = JSC020(**fake_sc)
                 session.add(sc)
                 session.commit()
+
+    print('students: {}, courses: {}, scs: {}.'.format(counter[0], counter[1], counter[2]))
 
 
 if __name__ == '__main__':

1d1a55717bae2c76ffbf36028574e01a9cdfe2ea
magicwenli <yxra3603@outlook.com>
2021-04-27 10:49:19 +0800

    重新组织了代码，更加简练了

--- extents.py
+++ extents.py
@@ -1,28 +1,28 @@
 import json
+import logging.handlers
 import re
 
 from faker import Faker
 
-Faker.seed(114)
-fake = Faker('zh_CN')
-
 
 class MyFaker():
+    Faker.seed(114)
+    fake = Faker('zh_CN')
+
     def student(self):
-        fake_stu = {}
-        fake_stu["sno"] = fake.unique.bothify(text='0#0#####')  # sno
-        if fake.boolean():
+        fake_stu = {"sno": self.fake.unique.bothify(text='0#0#####')}
+        if self.fake.boolean():
             fake_stu["sex"] = '男'  # sex
-            fake_stu["sname"] = fake.name_male()  # sname
+            fake_stu["sname"] = self.fake.name_male()  # sname
         else:
             fake_stu["sex"] = '女'  # sex
-            fake_stu["sname"] = fake.name_female()  # sname
-        fake_stu["bdate"] = fake.date_between("-23y", "-19y").strftime("%Y-%m-%d")  # bdate 年龄在19-23之间
-        fake_stu["height"] = "{:.2f}".format(fake.random_int(min=150, max=210) / 100)
-        fake_stu["dorm"] = fake.bothify(letters='东西南北', text="?") + "{}舍{}{:0>2d}".format(
-            fake.random_int(min=1, max=20),
-            fake.random_int(min=1, max=7),
-            fake.random_int(min=1, max=30))  # dorm
+            fake_stu["sname"] = self.fake.name_female()  # sname
+        fake_stu["bdate"] = self.fake.date_between("-23y", "-19y").strftime("%Y-%m-%d")  # bdate 年龄在19-23之间
+        fake_stu["height"] = "{:.2f}".format(self.fake.random_int(min=150, max=210) / 100)
+        fake_stu["dorm"] = self.fake.bothify(letters='东西南北', text="?") + "{}舍{}{:0>2d}".format(
+            self.fake.random_int(min=1, max=20),
+            self.fake.random_int(min=1, max=7),
+            self.fake.random_int(min=1, max=30))  # dorm
         return fake_stu
 
     def course(self, filepath):
@@ -46,3 +46,18 @@ class MyFaker():
                     cus_nos.append(a['KCH'])
                     cus_names.append(cus_name)
         return fake_cuss, cus_nos
+
+
+# set logger
+LOG_FILENAME = 'running.log'
+logger = logging.getLogger()
+
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+file_handler = logging.handlers.RotatingFileHandler(
+    LOG_FILENAME, maxBytes=10485760, backupCount=5, encoding="utf-8")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from faker import Faker
 from sqlalchemy.orm import sessionmaker
 
-from extents import MyFaker
+from extents import MyFaker, logger
 from models import db, base, JS020, JC020, JSC020
+
+DEBUG_MODE = 1
 
 
 def insertFake(my_fake, json_path):
@@ -11,66 +12,54 @@ def insertFake(my_fake, json_path):
     for i in range(1000):
         s_info = my_fake.student()
         students_no.append(s_info["sno"])
-
-        # 插入虚假学生信息
-        student = JS020(**s_info)
-        session.add(student)
-        session.commit()
+        if DEBUG_MODE == 1:
+            logger.info('student to be inserted: {}'.format(s_info))
+        else:
+            # 插入虚假学生信息
+            student = JS020(**s_info)
+            session.add(student)
+            session.commit()
 
     # fake course
     courses_info, courses_no = my_fake.course(json_path)
+    # courses_no 保存课程号，添加选课表时会用
     for c_info in courses_info:
-        # 插入虚假课程信息
-        course = JC020(**c_info)
-        session.add(course)
-        session.commit()
+        if DEBUG_MODE == 1:
+            logger.info('course to be inserted: {}'.format(c_info))
+        else:
+            # 插入虚假课程信息
+            course = JC020(**c_info)
+            session.add(course)
+            session.commit()
 
     # fake grade
     for sno in students_no:
         fake_sc = {}
-        cnos = fake.random_choices(elements=courses_no, length=fake.random_int(1, 10))
+        # 每个学生选1-10门课程
+        cnos = my_fake.fake.random_choices(elements=courses_no, length=my_fake.fake.random_int(1, 10))
         for cno in cnos:
             fake_sc["sno"] = sno
             fake_sc["cno"] = cno
-            grade = fake.random_int(0, 100)
-            if grade != 100 and fake.boolean():
+            # 成绩在[0,100]之间，step=0.5
+            grade = my_fake.fake.random_int(0, 100)
+            if grade != 100 and my_fake.fake.boolean():
                 grade += 0.5
             fake_sc["grade"] = "{:.1f}".format(grade)
 
-            sc = JSC020(**fake_sc)
-            session.add(sc)
-            session.commit()
+            if DEBUG_MODE == 1:
+                logger.info('sc to be inserted: {}'.format(fake_sc))
+            else:
+                # 插入虚假选课信息
+                sc = JSC020(**fake_sc)
+                session.add(sc)
+                session.commit()
 
-            print(fake_sc)
 
+if __name__ == '__main__':
+    my_fake = MyFaker()
+    Session = sessionmaker(db)
+    session = Session()
+    base.metadata.create_all(db)
 
-Faker(114)
-fake = Faker('zh_CN')
-
-Session = sessionmaker(db)
-session = Session()
-
-base.metadata.create_all(db)
-
-my_fake = MyFaker()
-json_path = 'src/course.json'
-insertFake(my_fake, json_path)
-
-# # Create
-# test={"sno":"1234567","sname":"peter"}
-# doctor_strange = Test(**test)
-# session.add(doctor_strange)
-# session.commit()
-
-# Read
-# tests = session.query(JS020)
-# for t in tests:
-#     print(t.sname)
-#
-# # Update
-# doctor_strange.title = "Some2016Film"
-# session.commit()
-#
-# # Delete
-# session.delete(doctor_strange)
-# session.commit()
+    json_path = 'src/course.json'
+    insertFake(my_fake, json_path)
